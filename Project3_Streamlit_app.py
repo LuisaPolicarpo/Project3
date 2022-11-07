ec519675783a7c9fc88fb92073ba681054f6587b
Luísa Colaço de Freitas <108865217+LuisaPolicarpo@users.noreply.github.com>
2022-11-07 15:25:17 +0000

    selectbox

--- Project3_Streamlit_app.py
+++ Project3_Streamlit_app.py
@@ -2,10 +2,13 @@ import pandas as pd
 import streamlit as st
 import pickle
 
-st.title('InovMovie')
+add_selectbox = st.sidebar.selectbox(
+    "cat:",
+    ["Cover","Frans", "Marta", "Luísa"])   
 
-
-image = Image.open('C:/Users/luisa/OneDrive/Ambiente de Trabalho/WCS/Project 3/movie.jpg')
-st.image(image)
+if add_selectbox == 'Cover':
+    st.title('InovMovie')
+    image = Image.open('C:/Users/luisa/OneDrive/Ambiente de Trabalho/WCS/Project 3/movie.jpg')
+    st.image(image)
     
-st.subheader('A new solution that can change the movie industry')
+    st.subheader('A new solution that can change the movie industry')

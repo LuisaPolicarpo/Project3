7b2fa2ee357a79e94cebbae116211d707ee566a5
Luísa Colaço de Freitas <108865217+LuisaPolicarpo@users.noreply.github.com>
2022-11-07 15:12:41 +0000

    Put an image and a subheader

--- Project3_Streamlit_app.py
+++ Project3_Streamlit_app.py
@@ -3,3 +3,9 @@ import streamlit as st
 import pickle
 
 st.title('InovMovie')
+
+
+image = Image.open('C:/Users/luisa/OneDrive/Ambiente de Trabalho/WCS/Project 3/movie.jpg')
+st.image(image)
+    
+st.subheader('A new solution that can change the movie industry')

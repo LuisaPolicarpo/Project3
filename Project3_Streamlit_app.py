46d037c85c41e9fa670f01d6e64beec056fac1ad
marta <martalbarbosa@gmail.com>
2022-11-09 15:50:35 +0000

    Update

--- Project3_Streamlit_app.py
+++ Project3_Streamlit_app.py
@@ -3,6 +3,9 @@ import streamlit as st
 import pickle
 from PIL import Image
 
+#Page configuration
+st.set_page_config(page_title=None, page_icon=None, layout="wide", initial_sidebar_state="auto", menu_items=None)
+
 #Background image:
 
 import base64
@@ -22,10 +25,48 @@ def add_bg_from_local(image_file):
     )
 add_bg_from_local('./Picture1.jpg')
 
-#Page configuration
-st.set_page_config(page_title=None, page_icon=None, layout="wide", initial_sidebar_state="auto", menu_items=None)
 
 #Define columns:
-col1, col2, col3, col4, col5 = st.columns(5)
+col1, col2, col3 = st.columns(3)
 
-st.title('InovMovie')
+with col1:
+    ('')
+    
+with col2:
+    st.markdown("<h1 style='text-align: center; color: White;'>InovMovie</h1>", unsafe_allow_html=True)
+    st.markdown("<h2 style='text-align: center; color: Steelblue;'>The best app for movie theatres</h1>", unsafe_allow_html=True)
+    
+    selected_movie_name = st.selectbox(
+    "Type or select a movie from the dropdown",
+     movies['title'].values
+    )
+
+    
+    
+with col3:
+    ('')
+    
+    session.options = st.multiselect(label="Select Movies", options=movies)
+
+    
+
+    
+#     session.slider_count = st.slider(label="movie_count", min_value=5, max_value=50)
+
+#     st.text("")
+#     st.text("")
+
+#     buffer1, col1, buffer2 = st.columns([1.45, 1, 1])
+
+#     is_clicked = col1.button(label="Recommend")
+
+#     if is_clicked:
+#     dataframe = recommend_table(session.options, movie_count=session.slider_count, tfidf_data=tfidf)
+
+#     st.text("")
+#     st.text("")
+#     st.text("")
+#     st.text("")
+
+#     if dataframe is not None:
+#         st.table(dataframe)

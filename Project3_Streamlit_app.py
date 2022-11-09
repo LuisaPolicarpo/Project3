d457305506164a12bcf65834d868bbfa3db42f8e
Marta barbosa <105504170+martabarbosa1@users.noreply.github.com>
2022-11-09 14:11:46 +0000

    update

--- Project3_Streamlit_app.py
+++ Project3_Streamlit_app.py
@@ -1,14 +1,1 @@
-import pandas as pd
-import streamlit as st
-import pickle
 
-add_selectbox = st.sidebar.selectbox(
-    "cat:",
-    ["Cover","Frans", "Marta", "Luísa"])   
-
-if add_selectbox == 'Cover':
-    st.title('InovMovie')
-    image = Image.open('C:/Users/luisa/OneDrive/Ambiente de Trabalho/WCS/Project 3/movie.jpg')
-    st.image(image)
-    
-    st.subheader('A new solution that can change the movie industry')

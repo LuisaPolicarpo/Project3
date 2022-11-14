5cea7eb011ac6f2d7e133a3e24474056c1d0e09a
marta <martalbarbosa@gmail.com>
2022-11-14 14:36:37 +0000

    Update with Frans code

--- Project3_Streamlit_app.py
+++ Project3_Streamlit_app.py
@@ -1,4 +1,3 @@
-### Import Libraries
 import pandas as pd
 
 # from sklearn.linear_model import LogisticRegression
@@ -8,23 +7,6 @@ import streamlit as st
 
 import pickle
 from PIL import Image
-import numpy as np 
-import re
-import seaborn as sns
-import matplotlib.pyplot as plt
-import matplotlib.ticker as mtick
-import re
-import nltk
-import spacy
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-from nltk.tokenize import sent_tokenize
-from nltk.stem import SnowballStemmer
-from string import punctuation
-from wordcloud import WordCloud
-
-### Import databases
-reviews_wc = pd.read_pickle("pickles/review_final-wc_p.pkl")
 
 
 
@@ -55,44 +37,12 @@ add_bg_from_local('./Picture1.jpg')
 col1, col2, col3 = st.columns(3)
 
 with col1:
-    genre_input = st.selectbox('Type a genre: ')
-    st.write('This is a re the most recurring words to describe ', genre_input, ' genre')
-    
-    ### Word Cloud for Genre with str contains
-    select_genre = str(input('Type :'))
-    text03 = reviews_wc[reviews_wc['genres'].str.contains(genre_input, na=False)]['review_content']
-    text03 = str(text03)
-    text03 = re.sub(r"\d+","",text03)
-    tokenizer = nltk.RegexpTokenizer(r"\w+")
-    nopunc = tokenizer.tokenize(text03)
-    words03=" ".join(nopunc)
-    words03= nltk.word_tokenize(words03.lower())
-    x3 = re.findall("[0-9]+",text03)
-    stop_words = set(stopwords.words('english'))
-    stop_words |= set(x3) #add the list with all the numbers in the string to the list of stopwords
-    stop_words.update({'n', 'name', 'review_content','length', 'dtype', 'object','movie','film','films','th','one','like','sharknado','b',genre_input.lower()})
-    words = words03
-
-    sentence04 = [w for w in words if not w in stop_words]
-
-    wordcloud = WordCloud(width=200, height=200,background_color='white', max_font_size=200, min_font_size=10, max_words=10)
-
-    sentence1 = sentence04
-    freq = nltk.FreqDist(sentence1)
-
-    wordcloud.generate_from_frequencies(freq)
-
-    plt.figure()
-    plt.imshow(wordcloud, interpolation="bilinear")
-    plt.axis("off")
-    plt.margins(x=0, y=0)
-    plt.show()
+    ('')
     
 with col2:
     st.markdown("<h1 style='text-align: center; color: White;'>InovMovie</h1>", unsafe_allow_html=True)
     st.markdown("<h2 style='text-align: center; color: Steelblue;'>The best app for movie theatres</h1>", unsafe_allow_html=True)
     
-<<<<<<< HEAD
     #Import table
     # df_inovmovie = pd.read_pickle('/content/drive/MyDrive/Colab Notebooks/Projects/Project3_my copy/title_basics_p.pkl')
     # movie_list = df['PrimaryTitle'].tolist()
@@ -104,12 +54,6 @@ with col2:
     #Selectbox for movie names
     text_input = st.selectbox('Movie title', df['col1'])
     # text_input = st.text_input('Movie title', autocomplete= df['col1'])  
-=======
-    # selected_movie_name = st.selectbox(
-    # "Type or select a movie from the dropdown",
-    #  movies['title'].values
-    # )
->>>>>>> cb5cf9ae07007a06bc3874dfac9975de0e3b219e
 
     if text_input:
         st.write("You entered: ", text_input)
@@ -130,10 +74,7 @@ with col3:
     ('')
     
     # session.options = st.multiselect(label="Select Movies", options=movies)
-<<<<<<< HEAD
 
-=======
->>>>>>> cb5cf9ae07007a06bc3874dfac9975de0e3b219e
 
 
     

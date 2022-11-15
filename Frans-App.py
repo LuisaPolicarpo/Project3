8ae5a6ef3a110fb62486c14e94797f10ccb3bb4d
Frans Grau <fransgrau01@gmail.com>
2022-11-15 15:02:27 +0000

    Update Frans-App.py

--- Frans-App.py
+++ Frans-App.py
@@ -25,68 +25,74 @@ reviews_wc = pd.read_pickle("pickles/review_final-wc_p.pkl")
 
 ### Define columns:
 st.set_page_config(page_title=None, page_icon=None, layout="wide", menu_items=None)
-# col1, col2, col3 = st.columns([2,1,1])
 
-# with col1:
-#     title = st.text_input('Type the title and press Enter')
-# if title:
-#         try: 
-#             url = f"https://www.omdbapi.com/?t={title}&apikey=38187759"
-#             re = requests.get(url)
-#             re = re.json()
-#             col1, col2, = st.columns([1,1])
-#             with col1:
-#                     st.image(re['Poster'])
-#             with col2:
-#                     st.subheader(re['Title'])
-#                     st.caption(f"Gender:{re['Genre']} Year: {re['Year']} ")
-#                     st.write (re['Plot'])
-#                     st.text(f"Rating: {re['imdbRating']}")
-#                     st.progress(float(re['imdbRating'])/10)
-#         except:
-#                 title = False
-#                 st.error('We currently don'')
-#                 contact_form = """
-#     <form action="https://formsubmit.co/inovmovie@gmail.com" method="POST">
-#      <input type="text" name="Suggestion" placeholder="Movie suggestion" required>
-#      <input type="text name="Name" placeholder="Your name" required>
-#      <button type="submit">Send</button>
-# </form>
-# """
-#                 st.markdown (contact_form, unsafe_allow_html=True)
+outer_cols = st.columns([2,0.5,2])
 
-# with col2:
-#     st.markdown('hola')
-
-# with col3:
-#     st.title('hola')
-
-outer_cols = st.columns([2,1,2])
-
+### Left side of screen
 with outer_cols[0]:
-    st.markdown('## Column 1')
-    st.selectbox('selectbox', [1,2,3], key='sel1')
-    
-    inner_cols = st.columns([1,1])
-    with inner_cols[0]:
-        st.markdown('Nested Column 1')
-        st.selectbox('selectbox', [1,2,3], key='sel2')
-    with inner_cols[1]:
-        st.markdown('Nested Column 2')
-        st.selectbox('selectbox', [1,2,3], key='sel3')
+    st.markdown('## Movie')
+    title = st.text_input('Type the title and press Enter')
+    if title:
+            try: 
+                url = f"https://www.omdbapi.com/?t={title}&apikey=38187759"
+                re = requests.get(url)
+                re = re.json()
 
-with outer_cols[1]:
-    st.markdown('')
-    
-with outer_cols[2]:
-    st.markdown('## Recommendations')
-    inner_cols = st.columns([1])
-    with inner_cols[0]:
-        st.markdown('Recomended 1')
-        st.selectbox('selectbox', [1,2,3], key='t2')
-    with inner_cols[0]:
-        st.markdown('Recomended 2')
-        st.selectbox('selectbox', [1,2,3], key='t1')
-    with inner_cols[0]:
-        st.markdown('Recomended 3')
-        st.selectbox('selectbox', [1,2,3], key='t3')
+                inner_cols = st.columns([1,2])
+                with inner_cols[0]:
+                    st.image(re['Poster'])
+
+                with inner_cols[1]:
+                    st.subheader(re['Title'])
+                    st.caption(f"Gender:{re['Genre']} Year: {re['Year']} ")
+                    st.write (re['Plot'])
+                    st.text(f"Rating: {re['imdbRating']}")
+                    st.progress(float(re['imdbRating'])/10)
+
+                        ### Right side of screen    
+                    with outer_cols[0]:
+                        st.markdown('')
+                        
+                    with outer_cols[2]:
+                        st.markdown('## Recommendations')
+                        inner_cols = st.columns([1,2])
+                        
+                        with inner_cols[0]:
+                            st.image(re['Poster'])
+                        
+                        with inner_cols[1]:
+                            st.subheader(re['Title'])
+                            st.write (re['Plot'])
+                            st.text(f"Rating: {re['imdbRating']}")
+                            st.progress(float(re['imdbRating'])/10)
+                        
+                        inner_cols = st.columns([1,2])
+                        with inner_cols[0]:
+                            st.image(re['Poster'])
+                        with inner_cols[1]:
+                            st.subheader(re['Title'])
+                            st.write (re['Plot'])
+                            st.text(f"Rating: {re['imdbRating']}")
+                            st.progress(float(re['imdbRating'])/10)
+                        
+                        inner_cols = st.columns([1,2])
+                        with inner_cols[0]:
+                            st.image(re['Poster'], use_column_width='auto')
+                        with inner_cols[1]:
+                            st.subheader(re['Title'])
+                            st.write (re['Plot'])
+                            st.text(f"Rating: {re['imdbRating']}")
+                            st.progress(float(re['imdbRating'])/10)
+
+            except:
+                title = False
+                st.markdown('''We currently don't have detailed information on this movie
+                drop us an email and our scouting team will look into it and will send you our review''')
+                contact_form = """
+                <form action="https://formsubmit.co/inovmovie@gmail.com" method="POST">
+                <input type="text" name="Suggestion" placeholder="Movie suggestion" required>
+                <input type="text name="Name" placeholder="Your name" required>
+                <button type="submit">Send</button>
+                </form>
+                """
+                st.markdown (contact_form, unsafe_allow_html=True)

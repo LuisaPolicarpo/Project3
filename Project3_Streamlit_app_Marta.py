172d0ccc0e9b4c14503f5bcfd7a6f0ae512bc3c6
marta <martalbarbosa@gmail.com>
2022-11-16 14:24:00 +0000

    ok

--- Project3_Streamlit_app_Marta.py
+++ Project3_Streamlit_app_Marta.py
@@ -1,32 +1,20 @@
-### Imports
-import streamlit as st
 import pandas as pd
-import numpy as np  
-import re
-import seaborn as sns
-import matplotlib.pyplot as plt
-import matplotlib.ticker as mtick
-import re
-import nltk
-import spacy
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-from nltk.tokenize import sent_tokenize
-from nltk.stem import SnowballStemmer
-from string import punctuation
-from wordcloud import WordCloud
-import pickle
-from PIL import Image
+
 # from sklearn.linear_model import LogisticRegression
 # from sklearn.model_selection import train_test_split
 
-### Import Data
-reviews_wc = pd.read_pickle("pickles/review_final-wc_p.pkl")
+import streamlit as st
 
-### Page configuration
+import pickle
+from PIL import Image
+
+
+
+#Page configuration
 st.set_page_config(page_title=None, page_icon=None, layout="wide", initial_sidebar_state="auto", menu_items=None)
 
-### Background image:
+#Background image:
+
 import base64
 def add_bg_from_local(image_file):
     with open(image_file, "rb") as image_file:
@@ -45,42 +33,11 @@ def add_bg_from_local(image_file):
 add_bg_from_local('./Picture1.jpg')
 
 
-### Define columns:
+#Define columns:
 col1, col2, col3 = st.columns(3)
 
 with col1:
-    genre_input = st.selectbox('Type a genre: ')
-    st.write('This is a re the most recurring words to describe ', genre_input, ' genre')
-    
-    ### Word Cloud for Genre with str contains
-    select_genre = str(input('Type :'))
-    text03 = reviews_wc[reviews_wc['genres'].str.contains(genre_input, na=False)]['review_content']
-    text03 = str(text03)
-    text03 = re.sub(r"\d+","",text03)
-    tokenizer = nltk.RegexpTokenizer(r"\w+")
-    nopunc = tokenizer.tokenize(text03)
-    words03=" ".join(nopunc)
-    words03= nltk.word_tokenize(words03.lower())
-    x3 = re.findall("[0-9]+",text03)
-    stop_words = set(stopwords.words('english'))
-    stop_words |= set(x3) #add the list with all the numbers in the string to the list of stopwords
-    stop_words.update({'n', 'name', 'review_content','length', 'dtype', 'object','movie','film','films','th','one','like','sharknado','b',genre_input.lower()})
-    words = words03
-
-    sentence04 = [w for w in words if not w in stop_words]
-
-    wordcloud = WordCloud(width=200, height=200,background_color='white', max_font_size=200, min_font_size=10, max_words=10)
-
-    sentence1 = sentence04
-    freq = nltk.FreqDist(sentence1)
-
-    wordcloud.generate_from_frequencies(freq)
-
-    plt.figure()
-    plt.imshow(wordcloud, interpolation="bilinear")
-    plt.axis("off")
-    plt.margins(x=0, y=0)
-    plt.show()
+    ('')
     
 with col2:
     st.markdown("<h1 style='text-align: center; color: White;'>InovMovie</h1>", unsafe_allow_html=True)
@@ -115,7 +72,11 @@ with col2:
     
 with col3:
     ('')
+    
     # session.options = st.multiselect(label="Select Movies", options=movies)
+
+
+
     
 #     session.slider_count = st.slider(label="movie_count", min_value=5, max_value=50)
 
@@ -151,3 +112,4 @@ with col3:
     # "Type or select a movie from the dropdown",
     #  df_inovmovie['PrimaryTitle'].values
     # )
+      

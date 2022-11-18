5a92484c1a51d3ecbe2025a9982239a1b5acded1
Luísa Colaço de Freitas <108865217+LuisaPolicarpo@users.noreply.github.com>
2022-11-18 12:23:19 +0000

    Last version

--- RecommendationSystem-App.py
+++ RecommendationSystem-App.py
@@ -12,6 +12,7 @@ from PIL import Image
 import requests
 import streamlit_nested_layout
 from streamlit_option_menu import option_menu
+# npm install streamlit-component-lib
 
 from sklearn.neighbors import NearestNeighbors
 
@@ -62,22 +63,8 @@ def rec(name, model):
 
 # ### STREAMLIT CODE
 st.set_page_config(page_title="InovMovie", page_icon="🎥", layout="wide", menu_items=None)
-# import base64
-# def add_bg_from_local(image_file):
-#     with open(image_file, "rb") as image_file:
-#         encoded_string = base64.b64encode(image_file.read())
-#     st.markdown(
-#     f"""
-#     <style>
-#     .stApp {{
-#         background-image: url(data:image/{"jpg"};base64,{encoded_string.decode()});
-#         background-size: cover
-#     }}
-#     </style>
-#     """,
-#     unsafe_allow_html=True
-#     )
-# add_bg_from_local('OneDrive/Documentos/GitHub/Project3/Images/Picture1.jpg')
+
+
 
 tab1, tab2 = st.tabs(["Recommendations", "Suggestions"])
 
